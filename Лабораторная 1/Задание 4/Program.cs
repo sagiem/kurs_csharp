521a564fa24c52fedf8c3fcb15521b01701c02c7
sagiem <sagiems@gmail.com>
2017-02-15 16:13:28 +0600

    сделал 4 и 5 задания

---
+++ Лабораторная 1/Задание 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messege;
+
+            Console.WriteLine("Введите текст");
+            messege = Console.ReadLine();
+
+            Console.WriteLine("Решение 1");
+            Console.WriteLine("Программирование это {0} !", messege);
+
+            Console.WriteLine("Решение 2");
+            Console.WriteLine("Программирование это " + messege + " !");
+
+            Console.WriteLine("Решение 3");
+            Console.Write("Программирование это ");
+            Console.Write(messege);
+            Console.WriteLine(" !");
+
+
+            
+
+
+        }
+    }
+}
---
+++ Лабораторная 1/Задание 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+
+            Console.WriteLine("Введите первую строку");
+            a = Console.ReadLine();
+            Console.WriteLine("Введите вторую строку");
+            b = Console.ReadLine();
+
+            Console.WriteLine("Решение 1");
+            Console.WriteLine("{1} {0}", a, b);
+
+            Console.WriteLine("Решение 2");
+            Console.WriteLine(b +" "+ a);
+
+            Console.WriteLine("Решение 3");
+            Console.Write(b);
+            Console.Write(" ");
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
