c82c095d2495950778ec20771cde675225606b91
sagiem <sagiems@gmail.com>
2017-02-17 12:27:15 +0600

    Доделал лабораторную

---
+++ Лабораторная 1/Задание 10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x, y;
+
+            Console.WriteLine("Введите значение x");
+            x = Console.ReadLine();
+
+            Console.WriteLine("Введите значение y");
+            y = Console.ReadLine();
+
+            Console.WriteLine("INSERT INTO points (x, y) VALUES ('{0}', '{1}');", x, y);
+
+
+        }
+    }
+}
