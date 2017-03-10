40d071d4370be4453a964a7c736120ef1343a6ae
sagiem <sagiems@gmail.com>
2017-03-10 14:55:13 +0600

    лабораторная 6, задача 25

--- Лабораторная 6/Задача 25/Program.cs
+++ Лабораторная 6/Задача 25/Program.cs
@@ -10,6 +10,42 @@ namespace Задача_25
     {
         static void Main(string[] args)
         {
+            int H, W, i=0, q=0, z=0;
+
+            Console.WriteLine("Введите H");
+            H = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите W");
+            W = int.Parse(Console.ReadLine());
+
+
+            z = H;
+            
+            
+            while(i<W)
+            {
+                while(q<H)
+                {
+                    Console.Write("+");
+
+                    q++;
+                }
+                
+
+                while(z>H)
+                {
+                    Console.Write("#");
+
+                    z--;
+
+                }
+
+                H--;
+                i++;
+                q = 0;
+                
+                
+                Console.WriteLine();
+            }
         }
     }
 }
