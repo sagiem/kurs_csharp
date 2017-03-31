e309e5fdb86c4eb06825ea088227087435dccc0f
sagiem <sagiems@gmail.com>
2017-03-31 10:42:35 +0600

    Доделал 7 лабораторную

---
+++ Лабораторная 7/Задача 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c = "abcdefwxyz";
+            int  index = 0;
+
+            Console.WriteLine("Введите a");
+            a = Console.ReadLine();
+            Console.WriteLine("Введите b");
+            b = Console.ReadLine();
+
+
+            int indexa = c.IndexOf(a);
+            int indexb = c.IndexOf(b);
+            if (indexa < 0 || indexb < 0)
+            {
+                Console.WriteLine("Сообщение об ошибке");
+                return;
+            }
+
+            if (indexa < indexb)
+            {
+                index = indexb - indexa - 1;
+            }
+
+            else
+            {
+                index = indexa - indexb - 1;
+            }
+
+
+
+            Console.WriteLine(index);
+        }
+    }
+}
---
+++ Лабораторная 7/Задача 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, st = "abcdefwxyz";
+
+            Console.WriteLine("Введите данные");
+            a = Console.ReadLine();
+
+            int x = st.IndexOf(a);
+
+            if (x >= 0)
+            {
+                Console.WriteLine("Содержится");
+            }
+
+            else
+            {
+                Console.WriteLine("Не содержится");
+
+            }
+
+
+        }
+    }
+}
