31cfb125236f65169fd541fafc8a310a0c55e316
sagiem <sagiems@gmail.com>
2017-04-05 14:03:07 +0600

    Лабораторная 9

---
+++ Лабораторная 9/Задача 3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите A");
+            int  a = int.Parse(Console.ReadLine());
+            string z;
+            int m = 0;
+
+            if(a<=0)
+            {
+                Console.WriteLine("Сообщение об ошибке");
+                return;
+            }
+
+            switch(a)
+            {
+                case 13:
+                    z = "test1.txt";
+                    break;
+                case 371:
+                    z = "test2.txt";
+                    break;
+                case 12:
+                    Console.WriteLine("Сообщение об ошибке");
+                    return;
+                case 23:
+                    z = "test5.txt";
+                    break;
+                default:
+                    Console.WriteLine("Сообщение об ошибке");
+                    return;
+
+            }
+
+            StreamReader reader = new StreamReader(z);
+
+            while(!reader.EndOfStream)
+            {
+               int y = int.Parse(reader.ReadLine());
+
+                if(y%a==0)
+                {
+                    m++;
+                }
+     
+            }
+
+            Console.WriteLine(m);
+
+        }
+    }
+}
---
+++ Лабораторная 9/Задача 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int x, y;
+            Console.WriteLine("Введите номер теста");
+            int a = int.Parse(Console.ReadLine());
+
+            switch(a)
+            {
+                case 1:
+                    t = "test1.txt";
+                    break;
+                case 2:
+                    t = "test2.txt";
+                    break;
+                case 3:
+                    t = "test3.txt";
+                    break;
+                default:
+                    Console.WriteLine("Сообщение об ошибке");
+                    return;
+            }
+
+            StreamReader reader = new StreamReader(t);
+
+            x = int.Parse(reader.ReadLine());
+            y = x;
+
+            while(!reader.EndOfStream)
+            {
+                x = int.Parse(reader.ReadLine());
+
+                if(x<y)
+                {
+                    y = x;
+                }
+
+            }
+
+            Console.WriteLine(y);
+        }
+    }
+}
