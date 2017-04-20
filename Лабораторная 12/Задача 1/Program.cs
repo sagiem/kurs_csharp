1befbbf1478d0ba1733767aaf32fdcf2130e46c2
sagiem <sagiems@gmail.com>
2017-04-20 15:49:53 +0600

    лабораторная 12

--- Лабораторная 12/Задача 1/Program.cs
+++ Лабораторная 12/Задача 1/Program.cs
@@ -17,7 +17,23 @@ namespace Задача_1
             task1860("цели");
             task1860("результату");
 
-            Console.WriteLine("7472");
+            Console.WriteLine("task4764");
+
+            task4764("трудности");
+            task4764("загадки");
+            task4764("сложные задачи");
+
+            Console.WriteLine("task2429");
+
+            task2429("просто");
+            task2429("интересно");
+            task2429("перспективно");
+
+            Console.WriteLine("task7472");
+
+            Console.WriteLine("task2862");
+
+            task2862();
 
             task7472("начало", "конец");
             task7472("сила", "знание");
@@ -30,6 +46,16 @@ namespace Задача_1
             task9231(-273.15);
             task9231(0.00006);
 
+            Console.WriteLine("task8624");
+
+            task8624(811);
+            task8624(1583);
+
+            Console.WriteLine("task2959");
+
+            task2959("123456789");
+            task2959("fa93f");
+
 
         }
 
@@ -38,14 +64,45 @@ namespace Задача_1
             Console.WriteLine("Мы стремимся к "+a);
         }
 
+        static void task4764(string a)
+        {
+            Console.WriteLine(a + " нас не остановят!");
+        }
+
+        static void task2429(string a)
+        {
+            Console.WriteLine("Программирование это "+a);
+        }
+
+
+
         static void task7472(string a, string b)
         {
             Console.WriteLine(b+" "+a);
+        }
+
+        static void task2862()
+        {
+            Console.WriteLine("{0:N4}",Math.PI);
+            Console.WriteLine("{0:N4}",Math.E);
         }
 
         static void task9231(double a)
         {
             Console.WriteLine("{0:N4}",a);
         }
+
+        static void task8624(int a)
+        {
+            Console.WriteLine("\"{0}\"",a);
+        }
+
+        static void task2959(string a)
+        {
+            Console.WriteLine("SELECT first_name, last_name, group FROM students WHERE student_id = '{0}'",a);
+        }
+    
+
+    
     }
 }
