23aad4ac6cf9596eb6ace2bae8145a5d2d3b7712
sagiem <sagiems@gmail.com>
2017-05-12 11:48:33 +0600

    1

---
+++ Лабораторная 6.1/Задача 22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] arr = s.Split();
+            string x;
+
+            for(int i =0; i<arr.Length; i++)
+            {
+                x = arr[i];
+            }
+        }
+    }
+}
