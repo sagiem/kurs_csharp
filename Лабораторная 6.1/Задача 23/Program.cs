69a3e883ea787d6ac237bce039ec5967a981234a
sagiem <sagiems@gmail.com>
2017-05-19 10:52:51 +0600

    доделал массивы

--- Лабораторная 6.1/Задача 23/Program.cs
+++ Лабораторная 6.1/Задача 23/Program.cs
@@ -13,32 +13,76 @@ namespace Задача_23
 
             string s = Console.ReadLine();
             string[] arr = s.Split();
-            int[] arr1 = new int[arr.Length];
-            int m = 0;
+            
 
-            for (int i = 0; i < arr.Length; i++)
+            HashSet<int> rng = new HashSet<int>();
+
+            for(int i = 0; i< arr.Length; i++)
             {
                 int x = int.Parse(arr[i]);
-                for (int l = 0; l < arr.Length; l++)
-                {
-                    if(x!=arr1[l])
-                    {
-                        m = x;
-                    }
-
-                }
-
-                arr1[i] = x;
-                m = 0;
-
-              
-
+                rng.Add(x);
             }
+
+            int[] arr1 = new int[rng.Count];
+            rng.CopyTo(arr1);
 
             foreach (int item in arr1)
             {
                 Console.Write(item + " ");
             }
+
+            
+          
+
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    int x = int.Parse(arr[i]);
+            //    for (int l = 0; l < arr.Length; l++)
+            //    {
+                    
+
+            //        if (x == arr1[l])
+            //        {
+            //            m++;
+            //        }
+
+            //    }
+
+            //    if (m == 0)
+            //    {
+            //        arr1[i] = x;
+            //    }
+            //    else
+            //    {
+            //        arr[i] = null;
+            //    }
+            //    m = 0; 
+
+            //}
+
+            //for (int k = 0; k < arr1.Length; k++ )
+            //{
+            //    if(arr1[k]!=null)
+            //    {
+            //        p++;
+            //    }
+            //}
+
+            //int[] arr2 = new int[p];
+
+            //for (int z = 0; z < arr1.Length; z++)
+            //{
+            //    if (arr1[z] != null)
+            //    {
+            //        arr2[z]=arr1[z];
+            //    }
+            //}
+
+
+            //    foreach (int item in arr2)
+            //    {
+            //        Console.Write(item + " ");
+            //    }
         }
     }
 }
--- Лабораторная 6.1/Задача 24/Program.cs
+++ Лабораторная 6.1/Задача 24/Program.cs
@@ -10,6 +10,19 @@ namespace Задача_24
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            string[] arr = s.Split();
+
+
+            HashSet<int> rng = new HashSet<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int x = int.Parse(arr[i]);
+                rng.Add(x);
+            }
+
+            Console.WriteLine(rng.Count);
         }
     }
 }
