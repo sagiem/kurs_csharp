9d5814585a1bdb5f2988433b82b8e7e0dac217ac
sagiem <sagiems@gmail.com>
2017-06-02 12:07:59 +0600

    классы

---
+++ Классы/Задача 19/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите номер теста");
+                string a = Console.ReadLine();
+                string filename = "test" + a + ".csv";
+                Raschet r = new Raschet();
+
+                try
+                {
+                    ProcessFile(filename, r);
+                    Console.WriteLine(r.Print());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.Write("действий:{0} ", r.m);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            public static void ProcessFile(string filename, Raschet r)
+            {
+                if (!File.Exists(filename))
+                {
+                    throw new ArgumentException("Файл не существует");
+                }
+
+                StreamReader reader = new StreamReader(filename);
+                while (!reader.EndOfStream)
+                {
+                    string row = (reader.ReadLine());
+                    string[] arr = row.Split(';');
+
+                    if (arr.Length != 2)
+                    {
+                        throw new ArgumentException("Некорректный формат");
+                    }
+                    int k = int.Parse(arr[1]);
+                    r.Selectaction(arr[0], k);
+                }
+            }
+        }
+    }
+}
