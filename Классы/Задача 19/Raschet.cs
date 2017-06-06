5a3197965a37795ab41589b3f497b276363f7f45
sagiem <sagiems@gmail.com>
2017-06-06 19:25:34 +0600

    классы

--- Классы/Задача 19/Raschet.cs
+++ Классы/Задача 19/Raschet.cs
@@ -8,93 +8,77 @@ namespace Задача_19
 {
     class Raschet
     {
-        Dictionary<string, string> dir = new Dictionary<string, string>();
+        Dictionary<string, Rectangle> dir = new Dictionary<string, Rectangle>();
 
-        public void Selectaction(string action)
+        public void Selectaction(string line)
         {
-            string[] arr = action.Split(';');
+            string[] arr = line.Split(';');
+            string action = arr[0];
 
             if(arr[0]== "create")
             {
+                if (arr.Length == 6)
+                {
+                    int x = int.Parse(arr[2]);
+                    int y = int.Parse(arr[3]);
+                    int w = int.Parse(arr[4]);
+                    int h = int.Parse(arr[4]);
+                    Rectangle Rec = new Rectangle(x, y, w, h);
+                    dir.Add(arr[1], Rec);
+                }
+                else
+                {
+                    throw new ArgumentException("Некорректный формат");
+                }
 
-                dir.Add(arr[1], (arr[2] + ';' + arr[3] + ';' + arr[4]) + ';' + arr[5]);
-
-                
 
             }
 
+            if (arr.Length != 3)
+            {
+                throw new ArgumentException("Некорректный формат");
+            }
 
+            if (!dir.ContainsKey(arr[1]))
+            {
+
+                throw new ArgumentException("Некорректный формат");
+            }
+
+            Rectangle figure = dir[arr[1]];
+            int value = int.Parse(arr[2]);
 
             if (action == "shiftX")
-            {
-                shiftX(value);
+            {              
+                figure.shiftX(value);
             }
             else if (action == "shiftY")
             {
-                shiftY(value);
+                figure.shiftY(value);
             }
             else if (action == "stretchX")
             {
-                stretchX(value);
+                figure.stretchX(value);
             }
             else if (action == "stretchY")
             {
-                stretchY(value);
+                figure.stretchY(value);
             }
 
             else
             {
                 throw new ArgumentException("Некорректное действие: " + action);
-            }
-            this.m++;
+            }    
         }
 
-        //public void shiftX(int a)
+
+
+        //public void Print()
         //{
-        //    this.cx = this.cx + a;
-        //    if (this.cx < this.r)
+        //    foreach (KeyValuePair<string, string> item in dir)
         //    {
-        //        throw new ArgumentException("Левая точка круга должна иметь неотрицательные координаты");
+        //        Console.WriteLine(item);
         //    }
         //}
-
-        //public void shiftY(int a)
-        //{
-        //    this.cy = this.cy + a;
-        //    if (this.cy < this.r)
-        //    {
-        //        throw new ArgumentException("Верхняя точка круга должна иметь неотрицательные координаты");
-        //    }
-        //}
-
-        //public void stretchX(int dw)
-        //{
-        //    this.r = this.r + dw / 2;
-        //    this.cx = this.cx + dw / 2;
-        //    this.cy = this.cy + dw / 2;
-        //    if (this.r < 0)
-        //    {
-        //        throw new ArgumentException("Ширина должна быть положительной");
-        //    }
-        //}
-
-        //public void stretchY(int dw)
-        //{
-        //    this.r = this.r + dw / 2;
-        //    this.cx = this.cx + dw / 2;
-        //    this.cy = this.cy + dw / 2;
-        //    if (this.r < 0)
-        //    {
-        //        throw new ArgumentException("Высота должна быть положительной");
-        //    }
-        //}
-
-        public void Print()
-        {
-            foreach (KeyValuePair<string, string> item in dir)
-            {
-                Console.WriteLine(item);
-            }
-        }
     }
 }
